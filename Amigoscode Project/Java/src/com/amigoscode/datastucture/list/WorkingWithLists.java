2ade9529be96883cd0eb631c14e67952c06e3bb4
Tahir Mehboob <tahirmehboob106@gmail.com>
2025-04-23 01:35:23 +0500

    list
    adding code in workingwithlist class

--- Amigoscode Project/Java/src/com/amigoscode/datastucture/list/WorkingWithLists.java
+++ Amigoscode Project/Java/src/com/amigoscode/datastucture/list/WorkingWithLists.java
@@ -1,4 +1,32 @@
 package com.amigoscode.datastucture.list;
 
+import java.util.ArrayList;
+import java.util.Collection.List;
+
 public class WorkingWithLists {
+
+
+    public static void main(String[] args) {
+
+        List<String> colors = new ArrayList();
+
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+
+        System.out.println(colors.get(1));
+        System.out.println(colors.get(2));
+
+        System.out.println();
+        System.out.println(colors.contains("yellow"));
+        System.out.println();
+        // print the element by using for each
+        colors.forEach(System.out::println);
+
+        for(String color : colors){
+            System.out.println(" \n "+color);
+        }
+
+
+    }
 }
